01f8091a7f9e5b6a6175bd6e3c6037dce8f67261
ssun0611 <rlathfdl463@naver.com>
2017-05-25 20:50:12 +0900

    #3  Change Dialog list and Event Programing

--- app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
@@ -1,5 +1,6 @@
 package rlathfdl463.kr.hs.emirim.dialogtest;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,12 +9,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     String[] itemArr={"김성철", "윤용만", "서인후", "이해기","준","카인즈","토비아스","벤볼리오","황국열","정세훈","청순","사담","박상구"};
+    Button butDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button butDialog=(Button) findViewById(R.id.but_dialog);
+        butDialog=(Button) findViewById(R.id.but_dialog);
         butDialog.setOnClickListener(this);
     }
 
@@ -23,9 +25,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         dialog.setTitle("해더니 남편 고르기");
         dialog.setIcon(R.drawable.tangguri);
         //dialog.setMessage("안녕? 나는 탕구리!");
-        dialog.setItems(itemArr,null);
-        dialog.setPositiveButton("안녕",null);
-        dialog.setNegativeButton("저리가",null);
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                butDialog.setText(itemArr[i]);
+                tos
+            }
+        });
+        dialog.setPositiveButton("지구 뿌셔 철컹철컹",null);
+
         dialog.show();
 
     }
