8c5ce9181038008cdf05c4fe9e17bf6c9be47850
ssun0611 <rlathfdl463@naver.com>
2017-05-29 18:55:50 +0900

    #4 change radio button

--- app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     String[] itemArr={"김성철", "윤용만", "서인후", "이해기","준","카인즈","토비아스","벤볼리오","황국열","정세훈","청순","사담","박상구"};
@@ -22,14 +23,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
-        dialog.setTitle("해더니 남편 고르기");
+        dialog.setTitle("세젤잘 고르기");
         dialog.setIcon(R.drawable.tangguri);
         //dialog.setMessage("안녕? 나는 탕구리!");
-        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+        /*dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 butDialog.setText(itemArr[i]);
-                tos
+                Toast.makeText(getApplicationContext(), "철컹쓰 뽀에버.", Toast.LENGTH_SHORT).show();
+            }
+        }); */
+
+        //라디오 버튼 목록
+        dialog.setSingleChoiceItems(itemArr,0,new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                butDialog.setText(itemArr[i]);
             }
         });
         dialog.setPositiveButton("지구 뿌셔 철컹철컹",null);
