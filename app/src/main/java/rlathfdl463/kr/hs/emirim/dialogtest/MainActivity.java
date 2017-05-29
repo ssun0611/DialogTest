76353ab58cdb06d2c8ff4d13b5528630717b4092
ssun0611 <rlathfdl463@naver.com>
2017-05-29 19:19:07 +0900

    #5 checkbox list

--- app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
+++ app/src/main/java/rlathfdl463/kr/hs/emirim/dialogtest/MainActivity.java
@@ -8,21 +8,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    String[] itemArr={"김성철", "윤용만", "서인후", "이해기","준","카인즈","토비아스","벤볼리오","황국열","정세훈","청순","사담","박상구"};
+import static rlathfdl463.kr.hs.emirim.dialogtest.R.id.but_dialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    String[] itemArr = {"김성철", "윤용만", "서인후", "이해기"};
     Button butDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        butDialog=(Button) findViewById(R.id.but_dialog);
+        butDialog = (Button) findViewById(but_dialog);
         butDialog.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("세젤잘 고르기");
         dialog.setIcon(R.drawable.tangguri);
         //dialog.setMessage("안녕? 나는 탕구리!");
@@ -35,15 +37,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }); */
 
         //라디오 버튼 목록
-        dialog.setSingleChoiceItems(itemArr,0,new DialogInterface.OnClickListener() {
+       /* dialog.setSingleChoiceItems(itemArr,0,new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 butDialog.setText(itemArr[i]);
             }
+        });*/
+        //체크 박스 목록
+        // 선택 여부 배열
+        boolean[] checkedItems = {true, false, true, false};
+        dialog.setMultiChoiceItems(itemArr, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i, boolean b) {
+                butDialog.setText(itemArr[i]);
+            }
         });
-        dialog.setPositiveButton("지구 뿌셔 철컹철컹",null);
-
         dialog.show();
-
     }
 }
+      //  dialog.setPositiveButton("지구 뿌셔 철컹철컹",null);
+
+        //dialog.show();
+
